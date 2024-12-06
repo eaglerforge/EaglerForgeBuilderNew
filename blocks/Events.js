d082ed4d4eeef7af87f8a7b6d1af7275e5e8ee06
OeildeLynx31 <vincent@top7box.com>
2024-12-06 19:21:47 +0100

    Add the events block category
    + fix shadow on texts that is making like a blur effect sometimes

---
+++ blocks/Events.js
@@ -0,0 +1,171 @@
+const keyCodes = [
+    ["ANY", ""],
+    ["SPACE", "32"],
+    ["UP ARROW", "38"],
+    ["DOWN ARROW", "40"],
+    ["LEFT ARROW", "37"],
+    ["RIGHT ARROW", "39"],
+    ["A", "65"],
+    ["B", "66"],
+    ["C", "67"],
+    ["D", "68"],
+    ["E", "69"],
+    ["F", "70"],
+    ["G", "71"],
+    ["H", "72"],
+    ["I", "73"],
+    ["J", "74"],
+    ["K", "75"],
+    ["L", "76"],
+    ["M", "77"],
+    ["N", "78"],
+    ["O", "79"],
+    ["P", "80"],
+    ["Q", "81"],
+    ["R", "82"],
+    ["S", "83"],
+    ["T", "84"],
+    ["U", "85"],
+    ["V", "86"],
+    ["W", "87"],
+    ["X", "88"],
+    ["Y", "89"],
+    ["Z", "90"],
+    ["0", "48"],
+    ["1", "49"],
+    ["2", "50"],
+    ["3", "51"],
+    ["4", "52"],
+    ["5", "53"],
+    ["6", "54"],
+    ["7", "55"],
+    ["8", "56"],
+    ["9", "57"],
+    ["F1", "112"],
+    ["F2", "113"],
+    ["F3", "114"],
+    ["F4", "115"],
+    ["F5", "116"],
+    ["F6", "117"],
+    ["F7", "118"],
+    ["F8", "119"],
+    ["F9", "120"],
+    ["F10", "121"],
+    ["F11", "122"],
+    ["F12", "123"],
+];
+
+Blockly.Blocks['events_onModLoads'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldLabel('when all mods finished loading'));
+        this.appendStatementInput('CODE')
+            .setAlign(Blockly.inputs.Align.CENTRE)
+            .appendField('do');
+        this.setColour(55);
+        this.setTooltip('Is executed when all mods finished loading.');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['events_onClientTick'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldLabel('each client tick'));
+        this.appendStatementInput('CODE')
+            .setAlign(Blockly.inputs.Align.CENTRE)
+            .appendField('do');
+        this.setColour(55);
+        this.setTooltip('Is executed on each client tick.');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['events_onClientFrame'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldLabel('each client frame'));
+        this.appendStatementInput('CODE')
+            .setAlign(Blockly.inputs.Align.CENTRE)
+            .appendField('do');
+        this.setColour(55);
+        this.setTooltip('Is executed on each client frame.');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['events_onKeyPressed'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField('when')
+            .appendField(new Blockly.FieldDropdown(keyCodes), 'KEYCODE')
+            .appendField('key pressed');
+        this.appendStatementInput('CODE')
+            .setAlign(Blockly.inputs.Align.CENTRE)
+            .appendField('do');
+        this.setColour(55);
+        this.setTooltip('Is executed when a key is pressed.');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['events_onKeyReleased'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField('when')
+            .appendField(new Blockly.FieldDropdown(keyCodes), 'KEYCODE')
+            .appendField('key released');
+        this.appendStatementInput('CODE')
+            .setAlign(Blockly.inputs.Align.CENTRE)
+            .appendField('do');
+        this.setColour(55);
+        this.setTooltip('Is executed when a key is released.');
+        this.setHelpUrl('');
+    }
+};
+
+javascript.javascriptGenerator.forBlock['events_onModLoads'] = function(block, generator) {
+    const statement = generator.statementToCode(block, 'CODE');
+    const code = `
+ModAPI.addEventListener("load", () => { 
+    ${statement} })`;
+    return code;
+}
+
+javascript.javascriptGenerator.forBlock['events_onClientTick'] = function(block, generator) {
+    const statement = generator.statementToCode(block, 'CODE');
+    const code = `
+ModAPI.addEventListener("update", () => { 
+    ${statement} })`;
+    return code;
+}
+
+javascript.javascriptGenerator.forBlock['events_onClientFrame'] = function(block, generator) {
+    const statement = generator.statementToCode(block, 'CODE');
+    const code = `
+ModAPI.addEventListener("frame", () => { 
+    ${statement} })`;
+    return code;
+}
+
+javascript.javascriptGenerator.forBlock['events_onKeyPressed'] = function(block, generator) {
+    const statement = generator.statementToCode(block, 'CODE');
+    const keyCode = block.getFieldValue('KEYCODE');
+    const code = `
+window.addEventListener("keydown", event => {
+    ${keyCode !== ""?"if (event.keyCode === '"+keyCode+"') {":""}
+    ${statement}
+    ${keyCode !== ""?"}":""}`;
+    return code;
+}
+
+javascript.javascriptGenerator.forBlock['events_onKeyReleased'] = function(block, generator) {
+    const statement = generator.statementToCode(block, 'CODE');
+    const keyCode = block.getFieldValue('KEYCODE');
+    const code = `
+window.addEventListener("keyup", event => {
+    ${keyCode !== ""?"if (event.keyCode === '"+keyCode+"') {":""}
+    ${statement}
+    ${keyCode !== ""?"}":""}`;
+    return code;
+}
