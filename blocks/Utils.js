d6ef127d481df30b522c1d3b0c73b351af4821a9
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2024-12-28 03:16:51 +0800

    add a or b operator

---
+++ blocks/Utils.js
@@ -0,0 +1,21 @@
+const logic_a_or_b = {
+    init: function () {
+        this.appendValueInput('A')
+            .setAlign(Blockly.inputs.Align.RIGHT);
+        this.appendValueInput('B')
+            .setAlign(Blockly.inputs.Align.RIGHT)
+            .appendField('otherwise');
+        this.setInputsInline(true)
+        this.setOutput(true, null);
+        this.setTooltip('Uses the first value if it exists, otherwise uses the second value. (JavaScript ?? operator)');
+        this.setHelpUrl('');
+        this.setColour(210);
+    }
+};
+Blockly.common.defineBlocks({ logic_a_or_b: logic_a_or_b });
+javascript.javascriptGenerator.forBlock['logic_a_or_b'] = function () {
+    const value_a = generator.valueToCode(block, 'A', javascript.Order.ATOMIC);
+    const value_b = generator.valueToCode(block, 'B', javascript.Order.ATOMIC);
+    const code = `((${value_a})??(${value_b}))`;
+    return [code, javascript.Order.NONE];
+}
