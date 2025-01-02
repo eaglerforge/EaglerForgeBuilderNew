2e39e6fecf1fdc14c5b0c0d819645ca0e63eacc8
ZXMushroom63 <robert.pirtea.junior@gmail.com>
2025-01-02 22:43:43 +0800

    another fix

--- primitives/CraftingRecipe.js
+++ primitives/CraftingRecipe.js
@@ -134,18 +134,18 @@ PRIMITIVES["recipe"] = {
             var $$itemStackFromBlockWithMeta = ModAPI.reflect.getClassById("net.minecraft.item.ItemStack").constructors[2];
             var $$itemStackFromItem = ModAPI.reflect.getClassById("net.minecraft.item.ItemStack").constructors[4];
             var $$recipeInternal = [];
-            Object.keys($$recipeLegend).forEach((key) => {
-                $$recipeInternal.push($$ToChar(key));
-                var ingredient = ($$recipeLegend[key].type === "block" ? $$itemStackFromBlockWithMeta(ModAPI.blocks[$$recipeLegend[key].id].getRef(),1,$$recipeLegend[key].meta) : ModAPI.items[$$recipeLegend[key].id].getRef());
-                $$recipeInternal.push(ingredient);
+            Object.keys($$recipeLegend).forEach(($$key) => {
+                $$recipeInternal.push($$ToChar($$key));
+                var $$ingredient = ($$recipeLegend[$$key].type === "block" ? $$itemStackFromBlockWithMeta(ModAPI.blocks[$$recipeLegend[$$key].id].getRef(),1,$$recipeLegend[$$key].meta) : ModAPI.items[$$recipeLegend[$$key].id].getRef());
+                $$recipeInternal.push($$ingredient);
             });
 
             var $$recipeContents = $$recipePattern.map(row => ModAPI.util.str(row));
             var $$recipe = ModAPI.util.makeArray($$ObjectClass, $$recipeContents.concat($$recipeInternal));
 
             var $$resultItem = $$resultItemArg.startsWith("block/") ?
-                $$itemStackFromBlockWithMeta(ModAPI.blocks[$$resultItemArg.replace("block/", "").split("@")[0]].getRef(),${this.tags.resultQuantity},$$resultItemArg.replace("block/", "").split("@")[1] || 0)
-                : $$itemStackFromItem(ModAPI.items[$$resultItemArg.replace("item/", "")].getRef(), ${this.tags.resultQuantity})
+                ($$itemStackFromBlockWithMeta(ModAPI.blocks[$$resultItemArg.replace("block/", "").split("@")[0]].getRef(),${this.tags.resultQuantity},$$resultItemArg.replace("block/", "").split("@")[1] || 0))
+                : ($$itemStackFromItem(ModAPI.items[$$resultItemArg.replace("item/", "")].getRef(), ${this.tags.resultQuantity}));
             
             (function (${modifyResultHandler.args.join(",")}) {${modifyResultHandler.code}})($$resultItem);
             
