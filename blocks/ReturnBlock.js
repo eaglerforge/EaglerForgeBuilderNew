5596247e21530e86aeb83255dc5605c1d23bfb64
BendieGames <bendiereal@gmail.com>
2025-03-24 16:49:09 -0400

    Update ReturnBlock.js

--- blocks/ReturnBlock.js
+++ blocks/ReturnBlock.js
@@ -14,7 +14,7 @@ Blockly.common.defineBlocks({ local_this: local_this });
 javascript.javascriptGenerator.forBlock['local_this'] = function () {
     return 'this';
 }
-const wait_package ={
+const wait_package = {
     import wait from 'https://cdn.jsdelivr.net/npm/wait@0.4.2/dist/wait.min.js';
 
 };
