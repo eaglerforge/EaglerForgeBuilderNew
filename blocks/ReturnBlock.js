c276f9a201d3e3f9d06b2ac31263423829defeca
BendieGames <bendiereal@gmail.com>
2025-03-24 17:25:39 -0400

    Update ReturnBlock.js

--- blocks/ReturnBlock.js
+++ blocks/ReturnBlock.js
@@ -71,7 +71,7 @@ Blockly.common.defineBlocks({ proc_wait: proc_wait });
 javascript.javascriptGenerator.forBlock['proc_wait'] = function () {
     const wait = (ms) => new Promise((res) => setTimeout(res, ms));
     const value = javascript.javascriptGenerator.valueToCode(this, 'VALUE', javascript.Order.ATOMIC);
-    const code = 'await' + 'wait(' + value + ')';
+    const code = 'await' + 'wait(' + value + ');';
     return code;
 }
 
