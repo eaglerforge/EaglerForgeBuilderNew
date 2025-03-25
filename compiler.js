14ef2e018f6a9423f9f7c59dc5896e07e053ab79
BendieGames <bendiereal@gmail.com>
2025-03-25 16:01:24 -0400

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,7 +1,3 @@
-function toFunctionName(str) {
-    return (/^[a-zA-Z_]/.test(str.replace(/[^a-zA-Z0-9_]/g, '')) ? '' : '_') + str.replace(/[^a-zA-Z0-9_]/g, '');
-}
-
 function getCompiledCode() {
     javascript.javascriptGenerator.init(workspace);
     let datablock_contents = "";
@@ -31,6 +27,11 @@ function getCompiledCode() {
     });
     workspace.getAllBlocks().forEach(block => {
         functionPrereqs = functionPrereqs.concat(getBlockLibs(block));
+        
+        // Add processing for event blocks
+        if (block.type.startsWith("event_")) {
+            datablock_contents += javascript.javascriptGenerator.blockToCode(block);
+        }
     });
     Object.keys(javascript.javascriptGenerator.functionNames_).forEach(fn => {
         prereq_contents += javascript.javascriptGenerator.definitions_[fn];
@@ -49,47 +50,3 @@ function getCompiledCode() {
 });
     `;
 }
-
-
-function exportMod() {
-    let output = getCompiledCode()
-    fileSave(output, "mod.js");
-}
-var efiBuild = null;
-function getEfiBuild() {
-    return new Promise((res,rej)=>{
-        const fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        fileInput.accept = ".html";
-        fileInput.addEventListener('change', (event) => {
-            const file = event.target.files[0];
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                if (!reader.result.includes("__eaglerforgeinjector_installation_flag__")) {
-                    if (window.confirm("This file is not a EaglerForgeInjector build, or is too old.\nOpen the EaglerForgeInjector page?")) {
-                        window.open("https://eaglerforge.github.io/EaglerForgeInjector");
-                    }
-                    rej();
-                }
-                efiBuild = reader.result;
-                res();
-            };
-            reader.readAsText(file);
-        });
-        fileInput.click();
-    });
-}
-async function runMod() {
-    var url = "data:text/javascript," + encodeURIComponent(getCompiledCode());
-    if (!efiBuild) {
-        await getEfiBuild();
-    }
-    var insp = document.querySelector("#inspector");
-    insp.srcdoc = efiBuild + `<script>eaglercraftXOpts.noInitialModGui = false; eaglercraftXOpts.Mods = ["${url}"];</script>`;
-    if (document.querySelector(".datablock[data-dtype=inspector]")) {
-        document.querySelector(".datablock[data-dtype=inspector]").click();
-    }
-}
-
-document.querySelector("#export").addEventListener("click", exportMod);
-document.querySelector("#run").addEventListener("click", runMod);
