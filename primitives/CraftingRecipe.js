efc266c6b7eb29da21cd280d9cc85d35b8c98e43
Death <21cookej@roundwoodpark.co.uk>
2025-05-06 13:54:41 +0100

    Update CraftingRecipe.js

--- primitives/CraftingRecipe.js
+++ primitives/CraftingRecipe.js
@@ -1,4 +1,3 @@
-//TODO: quantityDropped, onBlockDestroyedByExplosion, onBlockActivated
 PRIMITIVES["recipe"] = {
     name: "Crafting Recipe",
     uses: [],
@@ -8,18 +7,19 @@ PRIMITIVES["recipe"] = {
         slot1: VALUE_ENUMS.ABSTRACT_ITEM,
         slot2: VALUE_ENUMS.ABSTRACT_ITEM,
         lf0: VALUE_ENUMS.NEWLINE,
-        slot3: VALUE_ENUMS.ABSTRACT_ITEM,      result: VALUE_ENUMS.ABSTRACT_ITEM,
+        slot3: VALUE_ENUMS.ABSTRACT_ITEM,
         slot4: VALUE_ENUMS.ABSTRACT_ITEM,
         slot5: VALUE_ENUMS.ABSTRACT_ITEM,
         lf1: VALUE_ENUMS.NEWLINE,
-        slot6: VALUE_ENUMS.ABSTRACT_ITEM,        resultQuantity: 1, ModifyResult: VALUE_ENUMS.ABSTRACT_HANDLER + "CraftingRecipeModifyResult",
+        slot6: VALUE_ENUMS.ABSTRACT_ITEM,
         slot7: VALUE_ENUMS.ABSTRACT_ITEM,
         slot8: VALUE_ENUMS.ABSTRACT_ITEM,
         lf2: VALUE_ENUMS.NEWLINE,
         lf3: VALUE_ENUMS.NEWLINE,
+        result: VALUE_ENUMS.ABSTRACT_ITEM,
         lf4: VALUE_ENUMS.NEWLINE,
-
-        
+        resultQuantity: 1,
+        ModifyResult: VALUE_ENUMS.ABSTRACT_HANDLER + "CraftingRecipeModifyResult",
     },
     getDependencies: function () {
         const matchesList = new Set([].bake().dynamicConcat("block_advanced", "id", (x) => {
@@ -109,6 +109,13 @@ PRIMITIVES["recipe"] = {
                 const cell = row[x];
                 $$recipePattern += `${cell === "item/air" ? " " : uniqueTypesMap[cell]}`
             }
+
+            // Add result display on the middle row (slightly to the right)
+            if (y === Math.floor(newGrid.length / 2)) {
+                const resItem = this.tags.result.replace("item/", "").replace("block/", "");
+                $$recipePattern += `   => ${resItem}`;
+            }
+
             $$recipePattern += '"';
             $$recipePattern += ",";
         }
@@ -159,6 +166,7 @@ PRIMITIVES["recipe"] = {
     }
     ModAPI.dedicatedServer.appendCode($$registerRecipe);
     $$registerRecipe();
-})();`;
+})();
+`;
     }
-}
+};
