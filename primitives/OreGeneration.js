069469aefd84c3390788c703de2c688563d80fa0
Death <21cookej@roundwoodpark.co.uk>
2025-09-05 14:02:13 +0100

    Update OreGeneration.js

--- primitives/OreGeneration.js
+++ primitives/OreGeneration.js
@@ -1,3 +1,4 @@
+//TODO: quantityDropped, onBlockDestroyedByExplosion, onBlockActivated
 PRIMITIVES["ore_generation"] = {
     name: "Ore Generation",
     type: "ore_generation",
@@ -7,8 +8,7 @@ PRIMITIVES["ore_generation"] = {
         veinSize: 4,
         veinCount: 105,
         minGenerationHeight: 0,
-        maxGenerationHeight: 256,
-        dimensionId: 0  // 0 = Overworld, -1 = Nether, 1 = End
+        maxGenerationHeight: 256
     },
     getDependencies: function () {
         const matchesList = new Set([].bake().dynamicConcat("block_advanced", "id", (x) => {
@@ -26,44 +26,25 @@ PRIMITIVES["ore_generation"] = {
     },
     asJavaScript: function () {
         var blockId = this.tags.oreBlock.replaceAll("block/", "").split("@")[0];
-        var blockMeta = parseInt(this.tags.oreBlock.replaceAll("block/", "").split("@")[1]) || 0;
+        var blockMeta = parseInt(this.tags.oreBlock.replaceAll("block/", "").split("@")[0]) || 0;
         var salt = "XXXXXX".split("").map(x => Math.floor(Math.random() * 10)).join("");
         return `(function OreGenerationDatablock() {
     ModAPI.dedicatedServer.appendCode(()=>{
-        const WorldGenMineable = ModAPI.reflect.getClassById("net.minecraft.world.gen.feature.WorldGenMinable").constructors.find(x=>x.length===3);
+        const WorldGenMineable = ModAPI.reflect.getClassById("net.minecraft.world.gen.feature.WorldGenMinable").constructors.find(x=>x.length===2);
+
         const BiomeDecorator_decorate = ModAPI.util.getMethodFromPackage("net.minecraft.world.biome.BiomeDecorator", "decorate");
         const oldDecorate = ModAPI.hooks.methods[BiomeDecorator_decorate];
         ModAPI.hooks.methods[BiomeDecorator_decorate] = function ($this, $world, $random, $biomeGenBase, $blockpos) {
             if (!$this.$currentWorld) {
-                // Cache dimensionId safely from world provider
-                $this.$efb2_dimensionId = $world.field_73011_w.field_76574_g;
-
-                let targetBlock;
-                if ($this.$efb2_dimensionId === -1) {
-                    targetBlock = ModAPI.blocks.netherrack.getDefaultState().getRef(); // Nether
-                } else if ($this.$efb2_dimensionId === 1) {
-                    targetBlock = ModAPI.blocks.end_stone.getDefaultState().getRef(); // End
-                } else {
-                    targetBlock = ModAPI.blocks.stone.getDefaultState().getRef(); // Overworld
-                }
-
-                $this[\`$efb2__${blockId + blockMeta}_${salt}_BlockGen\`] =
-                    WorldGenMineable(ModAPI.blocks[\`${blockId}\`].getStateFromMeta(${blockMeta}).getRef(),
-                                     ${this.tags.veinSize},
-                                     targetBlock);
+                $this[\`$efb2__${blockId + blockMeta}_${salt}_BlockGen\`] = WorldGenMineable(ModAPI.blocks[\`${blockId}\`].getStateFromMeta(${blockMeta}).getRef(), ${this.tags.veinSize});
             }
             return oldDecorate.apply(this, [$this, $world, $random, $biomeGenBase, $blockpos]);
         }
+
         const BiomeDecorator_generateOres = ModAPI.util.getMethodFromPackage("net.minecraft.world.biome.BiomeDecorator", "generateOres");
         const oldGenerateOres = ModAPI.hooks.methods[BiomeDecorator_generateOres];
         ModAPI.hooks.methods[BiomeDecorator_generateOres] = function ($this) {
-            // Use cached dimensionId instead of unsafe field access
-            if ($this.$efb2_dimensionId === ${this.tags.dimensionId}) {
-                $this.$genStandardOre1(${this.tags.veinCount},
-                    $this[\`$efb2__${blockId + blockMeta}_${salt}_BlockGen\`] || null,
-                    ${this.tags.minGenerationHeight},
-                    ${this.tags.maxGenerationHeight});
-            }
+            $this.$genStandardOre1(${this.tags.veinCount}, $this[\`$efb2__${blockId + blockMeta}_${salt}_BlockGen\`] || null, ${this.tags.minGenerationHeight}, ${this.tags.maxGenerationHeight});
             return oldGenerateOres.apply(this, [$this]);
         }
     });
